7bd558ef6ff9649379ef06e1ba22d7d209a59e76
shoxruxyarashov <yarashovshokhrukh@gmail.com>
2026-02-01 02:26:24 +0500

    entity add warehouse

---
+++ Data/Entities/WareHouse.cs
@@ -0,0 +1,10 @@
+namespace Data.Entities;
+public class WareHouse : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Location { get; set; }
+    public string? Manager { get; set; }
+    public string? Logo { get; set; }
+    public string CreatedBy { get; set; } = string.Empty;
+    
+}
